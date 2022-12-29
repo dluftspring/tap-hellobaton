57293eca3d35328027faa14329bf31482710dc82
dluftspring <daniel.luftspring@gmail.com>
2022-12-29 11:03:36 -0500

    Sync script from swagger spec

--- scripts/swagger_sync.py
+++ scripts/swagger_sync.py
@@ -4,16 +4,17 @@ Command line utility to generate schema files from the baton swagger open API sp
 
 import glob
 import json
+import ast
 import operator
 from os import PathLike
-from typing import List, Mapping, Tuple, Generator
+from typing import Any, Iterable, List, Mapping, Tuple, Generator
 from pathlib import Path
 from functools import reduce
 from jsonschema import Draft7Validator
+from .templates import STREAM_TEMPLATE
 import requests
 import dictdiffer
 import pandas as pd
-import tabulate
 import click
 
 
@@ -46,7 +47,7 @@ class OpenApiSpec(object):
 
         json_schema = dict(required=list(), type='object', properties=dict())
         json_schema['required'] = openapi_schema.get('required', list())
-        
+
         # restrict openAPI schema metadata we extract
         valid_keys = ['type', 'format']
         for key, value in openapi_schema['properties'].items():
@@ -73,14 +74,14 @@ class OpenApiSpec(object):
             # We have to add format only where it exists. Not all entries of json schema have a format string
             if format:
                 json_schema['properties'][key]['format'] = format
-            
+
             # Same idea as above but for the properties of complex json objects like arrays and nested json blobs
             if properties:
                 json_schema['properties'][key]['properties'] = properties
 
 
         return json_schema
-            
+
 
     def convert_to_schemas(self) -> Mapping[str, dict]:
 
@@ -104,7 +105,7 @@ class OpenApiSpec(object):
 
                         # This check should return None if it passes
                         assert Draft7Validator.check_schema(valid_schema) is None, "Schema {} is not valid".format(schema_key)
-  
+
         return schemas
 
 class SchemaUpdater(object):
@@ -119,6 +120,8 @@ class SchemaUpdater(object):
         self.existing_schemas: Mapping[str, dict] = self._read_schemas()
         self.allowed_changes: List[str] = allowed_changes
         self.updated_schemas: Mapping[str, dict] = { k:{} for k in self.new_schemas.keys() }
+        self.list_of_modified_schemas: List[str] = list()
+        self.list_of_new_schemas: List[str] = list()
 
     def _read_schemas(self) -> Mapping[str, dict]:
         """
@@ -130,17 +133,35 @@ class SchemaUpdater(object):
             with open(schema_file) as f:
                 dict_key = schema_file.split('/')[-1].split('.')[0]
                 schemas[dict_key] = json.load(f)
-        
+
         return schemas
-    
+
+    def _track_schema_changes(self, json_path: str, raw_path: str) -> None:
+        """
+        Tracking whether or not schemas are being updated or newly created
+        based on the output dictdiffer.diff(<old>, <new>)
+        """
+        if isinstance(json_path, list):
+                schema = json_path[0]
+            #@TODO - clunky to construct this then split it
+        else:
+            schema = json_path.split('.')[0]
+
+        if schema not in self.list_of_modified_schemas:
+            #@TODO - a bit clunky to check for this twice
+            if not raw_path:
+                self.list_of_new_schemas.append(schema)
+            self.list_of_modified_schemas.append(schema)
+
+
     def _process_changes(self, diff: Generator[Tuple, None, None]) -> pd.DataFrame:
 
         """
         Process the incoming changeset into a more readable dataframe with the follow structure
 
-        | type | path | change |
-        |-------|-----|--------|
-        | add  | actor.name | John Doe |
+        | type | path | change | old_value | new_value |
+        |------|------|--------|-----------|-----------|
+        | add  | actor.name | <raw changes> | None | John Doe |
 
         where
             type - the type of change e.g. add, change, remove
@@ -154,34 +175,36 @@ class SchemaUpdater(object):
         for t, p, c in diff:
             if t in ['add', 'remove']:
                 change_type = t
-                
+
                 for change in c:
                     # If a completely new schema is being added, the path is going to be missing or root so we get the path from the first entry of the changeset
-                    json_path = '.'.join([p, change[0]]) if p else change[0]
+                    print(p, change)
+                    json_path = '.'.join([p, str(change[0])]) if p else change[0]
                     new_value = None if t == 'remove' else change[1]
                     old_value = None if t == 'add' else change[1]
+                    self._track_schema_changes(json_path, p)
 
             elif t == 'change':
                 change_type = t
                 json_path = p
-                
-            
+                old_value = c[0]
+                new_value = c[1]
             else:
                 raise ValueError("Unknown change type {}".format(t))
-            
+
             init_dict['type'].append(change_type)
             init_dict['path'].append(json_path)
             init_dict['old_value'].append(old_value)
             init_dict['new_value'].append(new_value)
-        
+
         diff_df = pd.DataFrame(init_dict)
         return diff_df
-        
+
     def update_schemas(self) -> None:
         """
         Update schemas and write to a new directory called schemas
         """
-        
+
         for key in self.existing_schemas.keys():
             self.updated_schemas[key] = self.existing_schemas[key]
 
@@ -202,7 +225,7 @@ class SchemaUpdater(object):
 
         :returns: A list of keys that should be updated with the values in self.new_schemas
         """
-        
+
         raw_diff = dictdiffer.diff(self.existing_schemas, self.new_schemas)
         keys_to_update = []
 
@@ -213,16 +236,16 @@ class SchemaUpdater(object):
                     if not path:
                         dict_path = change[0]
                     keys_to_update.append(dict_path)
-        
+
         return keys_to_update
-    
+
     def change_report(self) -> None:
         """
         Print a report of the changes being written into the schemas directory
         """
         raw_diff = dictdiffer.diff(self.existing_schemas, self.new_schemas)
         diff = self._process_changes(raw_diff)
-        
+
         print("INFO - Your allowed changes are {}".format(self.allowed_changes))
         print(diff.loc[diff.type.isin(self.allowed_changes)].to_markdown(index=False))
 
@@ -233,21 +256,118 @@ class AstSchemaTemplate(object):
     """
     AST template class required for safely modifying this repositories python files
     """
-    def __init__(self):
-        pass
+    def __init__(self, new_streams: List[str]):
+        self.stream_template: str = STREAM_TEMPLATE
+        self.stream_path: PathLike = Path(__file__).parent.parent / Path('tap_hellobaton') / Path('streams.py')
+        self.tap_path: PathLike = Path(__file__).parent.parent / Path('tap_hellobaton') / Path('tap.py')
+        self.new_streams = new_streams
+
+    def read_streams(self) -> ast.Module:
+        """
+        Reads the streams.py file and returns the contents of the abstract syntax tree
+        """
+        with open(self.stream_path) as f:
+            return ast.parse(f.read())
+
+    def read_tap(self) -> ast.Module:
+        """
+        Reads the tap.py file and returns the contents
+        """
+        with open(self.tap_path) as f:
+            return ast.parse(f.read())
+
+    def update_streams(self) -> None:
+        """
+        Updates the tap stream objects in place
+        """
+        stream_tree = self.read_streams()
+
+        for new_stream in self.new_streams:
+            stream_object_name = self._snake_to_pascal(new_stream)
+            stream_name = new_stream
+            unparsed_class_def = self.stream_template.format(
+                stream_name=stream_name,
+                stream_object_name=stream_object_name
+                )
+            #Dump the contents into the ast syntax tree
+            stream_tree.body.append(ast.parse(unparsed_class_def))
+
+        with open(self.stream_path, 'w') as out:
+            out.write(ast.unparse(stream_tree))
+
+
+    def update_tap(self) -> None:
+        """
+        Updates the tap object in place. It's really important that we initialize the proper
+        ast objects to avoid a compilation error. This is why we don't template this out
+        """
+        tap_tree = self.read_tap()
+
+        for new_stream in self.new_streams:
+            object_stream_name = self._snake_to_pascal(new_stream)
+            idx = self._find_tap_import_index(tap_tree)
+            #Edit the import statement
+            tap_tree.body[idx].names.append(ast.alias(name=object_stream_name))
+            #Edit the assignment statement
+            tap_tree.body[idx+1].value.elts.append(ast.Name(object_stream_name,ast.Load))
+
+        with open(self.tap_path, 'w') as out:
+            out.write(ast.unparse(tap_tree))
+
+    def update(self) -> None:
+        """
+        Updates the streams.py and tap.py files in place.
+        """
+        self.update_streams()
+        self.update_tap()
+
+    def _snake_to_pascal(self, s: str) -> str:
+        """
+        Converts a snake case string to pascal case
+
+        >>> _snake_to_pascal('hello_world')
+        'HelloWorld'
+        """
+        return s.replace("_"," ").title().replace(" ","")
+
+    def _find_tap_import_index(self, tree: ast.Module) -> int:
+        """
+        Helper method that parses a generic tap syntax tree and returns
+        the indexer for the import statement which looks something like
+
+        from <tap>.streams import [
+            Stream1,
+            Stream2,
+        ]
+
+        This is the import we are trying to modify in place
+        """
+
+        #Our premise is that the generic import must be followed by an assignment
+        #There are more exhaustive ways to find these statments but this should work
+        #Most of the time
+        for i, obj in enumerate(tree.body):
+            if isinstance(obj, ast.ImportFrom):
+                if isinstance(tree.body[i+1], ast.Assign):
+                    return i
+
 
 @click.command()
 def main():
     """
     Update schemas in the schemas directory based on the current swagger spec and open api definition
     """
-    
+
     spec = OpenApiSpec()
     incoming_schemas = spec.convert_to_schemas()
 
     updater = SchemaUpdater(incoming_schemas)
-    print(f"Updating schemas... \n{updater.change_report()}")
+    print(f"Updating schemas with changes... \n{updater.change_report()}")
     updater.update_schemas()
+
+    code_generator = AstSchemaTemplate(new_streams = updater.list_of_new_schemas)
+    print(f"Generating new stream objects... \n{updater.list_of_new_schemas}")
+    code_generator.update()
 
 if __name__ == "__main__":
     main()
--- tap_hellobaton/streams.py
+++ tap_hellobaton/streams.py
@@ -1,118 +1,144 @@
 """Stream type classes for tap-hellobaton."""
-
 from pathlib import Path
 from tap_hellobaton.client import hellobatonStream
-
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+SCHEMAS_DIR = Path(__file__).parent / Path('./schemas')
 
 class ProjectsStream(hellobatonStream):
     """Define custom stream."""
-    name = "projects"
-    path = "/projects/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "projects.json"
+    name = 'projects'
+    path = '/projects/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'projects.json'
 
 class CompaniesStream(hellobatonStream):
     """Define custom stream."""
-    name = "companies"
-    path = "/companies/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "companies.json"
+    name = 'companies'
+    path = '/companies/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'companies.json'
 
 class MilestonesStream(hellobatonStream):
     """Define custom stream."""
-    name = "milestones"
-    path = "/milestones/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "milestones.json"
+    name = 'milestones'
+    path = '/milestones/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'milestones.json'
 
 class PhasesStream(hellobatonStream):
     """Define custom stream."""
-    name = "phases"
-    path = "/phases/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "phases.json"
+    name = 'phases'
+    path = '/phases/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'phases.json'
 
 class ProjectAttachementsStream(hellobatonStream):
     """Define custom stream."""
-    name = "project_attachments"
-    path = "/project_attachments/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "project_attachments.json"
+    name = 'project_attachments'
+    path = '/project_attachments/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'project_attachments.json'
 
 class ProjectUsersStream(hellobatonStream):
     """Define custom stream."""
-    name = "project_users"
-    path = "/project_users/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "project_users.json"
+    name = 'project_users'
+    path = '/project_users/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'project_users.json'
 
 class TasksStream(hellobatonStream):
     """Define custom stream."""
-    name = "tasks"
-    path = "/tasks/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "tasks.json"
+    name = 'tasks'
+    path = '/tasks/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'tasks.json'
 
 class TaskAttachmentsStream(hellobatonStream):
     """Define custom stream."""
-    name = "task_attachments"
-    path = "/task_attachments/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "task_attachments.json"
+    name = 'task_attachments'
+    path = '/task_attachments/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'task_attachments.json'
 
 class TemplatesStream(hellobatonStream):
     """Define custom stream."""
-    name = "templates"
-    path = "/templates/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "templates.json"
+    name = 'templates'
+    path = '/templates/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'templates.json'
 
 class TimeEntriesStream(hellobatonStream):
     """Define custom stream."""
-    name = "time_entries"
-    path = "/time_entries/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "time_entries.json"
+    name = 'time_entries'
+    path = '/time_entries/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'time_entries.json'
 
 class UsersStream(hellobatonStream):
     """Define custom stream."""
-    name = "users"
-    path = "/users/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "users.json"
+    name = 'users'
+    path = '/users/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'users.json'
 
 class ActivityStream(hellobatonStream):
     """Define custom stream."""
-    name = "activity"
-    path = "/activity/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "activity.json"
+    name = 'activity'
+    path = '/activity/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'activity.json'
 
 class CommentsStream(hellobatonStream):
     """Define custom stream."""
-    name = "comments"
-    path = "/comments/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "comments.json"
+    name = 'comments'
+    path = '/comments/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'comments.json'
 
 class CustomFieldsStream(hellobatonStream):
     """Define custom stream."""
-    name = "custom_fields"
-    path = "/custom_fields/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "custom_fields.json"
+    name = 'custom_fields'
+    path = '/custom_fields/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'custom_fields.json'
 
 class CustomFieldOptionsStream(hellobatonStream):
     """Define custom stream."""
-    name = "custom_field_options"
-    path = "/custom_field_options/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "custom_field_options.json"
+    name = 'custom_field_options'
+    path = '/custom_field_options/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'custom_field_options.json'
 
 class CustomFieldValuesStream(hellobatonStream):
     """Define custom stream."""
-    name = "custom_field_values"
-    path = "/custom_field_values/"
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "custom_field_values.json"
+    name = 'custom_field_values'
+    path = '/custom_field_values/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'custom_field_values.json'
+
+class ExternalTasks(hellobatonStream):
+    """Define custom stream."""
+    name = 'external_tasks'
+    path = '/external_tasks/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'external_tasks.json'
+
+class MilestoneFeedback(hellobatonStream):
+    """Define custom stream."""
+    name = 'milestone_feedback'
+    path = '/milestone_feedback/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'milestone_feedback.json'
+
+class TaskDeliverables(hellobatonStream):
+    """Define custom stream."""
+    name = 'task_deliverables'
+    path = '/task_deliverables/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'task_deliverables.json'
+
+class ProjectPhases(hellobatonStream):
+    """Define custom stream."""
+    name = 'project_phases'
+    path = '/project_phases/'
+    primary_keys = ['id']
+    schema_filepath = SCHEMAS_DIR / 'project_phases.json'
